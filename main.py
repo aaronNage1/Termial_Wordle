8ef01792b2fbaea8d6dbec41d61a625d40bb0fe7
Aaron Nagel <Aaronnagel2727@gmail.com>
2024-07-25 19:28:36 -0400

    Update main.py
    Fixed small bug and cleaned up some functions

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+#Termial Wordle
 
+#By Aaron
+#07/23/24
 
-welcome_msg = "Welcome to Aaron's Version of Wordle!\n\nThis is just wordle without the GUI...\n\n For instructions to play press \"Y\", press \"N\" to skip."
-print(welcome_msg)
-
-
-details = input()
-details_err_msg = "Can you read?!"
-print(details)
-if details != "Y" or details != "y" or details != "N" or details != "n":
-    print(details_err_msg)
+def instructions():
+    instr_msg = "\nYou have 6 guesses to correctly guess a 5 letter word.\nEach guess you make will tell you information on what the word could be.\n\
+        If you guessed a correct letter, this will appear in the correct letters list below each guess.\nIf the correct letter in the correct space, this will appear in the word progress bar below.\n"
+    print(instr_msg)
+    return
 
 def pick_random_word ():
     our_word = "Hello"
@@ -21,19 +20,19 @@ def start_game ():
 
 
 def make_your_guess (answer):
-    for x in range (6):
-        player_input = "You are on guess " + str(x) + " out of 6:"
-        guess = input(player_input)
-        check_guess(guess, answer)
+        for x in range (6):
+            player_input = "You are on guess " + str(x) + " out of 6:"
+            guess = input(player_input)
+            check_guess(guess, answer)
 
-    out_of_turns()
+        out_of_turns()
 
 
 def check_guess(guess, answer):
     if len(guess)!=5:
         print("Error, you must guess a 5 letter word.")
 
-    if guess == answer:
+    if guess.upper() == answer.upper():
         correct_guess()
 
     
@@ -43,5 +42,15 @@ def correct_guess():
 def out_of_turns():
     print("You lost, unfortunate....Get good.")
 
+welcome_msg = "Welcome to Aaron's Version of Wordle!\n\nThis is just wordle without the GUI...\n\nFor instructions to play press \"Y\", press \"N\" to skip."
+print(welcome_msg)
 
-start_game()# Wordle
+details = input()
+details_err_msg = "Can you read?!"
+
+if details.upper() != "Y" and details.upper() != "N":
+    print(details_err_msg)
+elif details.upper() == "Y":
+    instructions()
+
+start_game()
