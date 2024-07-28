e5830834f61cc035573e69baf36fd64694f998ec
Aaron Nagel <Aaronnagel2727@gmail.com>
2024-07-28 19:20:46 -0400

    Update main.py
    First work on correct letters function

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 def instructions():
     instr_msg = "\nYou have 6 guesses to correctly guess a 5 letter word.\nEach guess you make will tell you information on what the word could be.\n\
-        If you guessed a correct letter, this will appear in the correct letters list below each guess.\nIf the correct letter in the correct space, this will appear in the word progress bar below.\n"
+    If you guessed a correct letter, this will appear in the correct letters list below each guess.\nIf the correct letter in the correct space, this will appear in the word progress bar below.\n"
     print(instr_msg)
     return
 
@@ -31,10 +31,27 @@ def make_your_guess (answer):
 def check_guess(guess, answer):
     if len(guess)!=5:
         print("Error, you must guess a 5 letter word.")
+        return
+    guess = guess.upper()
+    answer = answer.upper()
 
-    if guess.upper() == answer.upper():
+    if guess == answer:
         correct_guess()
+        return
 
+    num_letters = 0
+    correct_letters = ""
+    for i in range(5):
+        for j in range (5):
+            if guess[i] == answer[j]:
+                correct_letters = correct_letters + guess[i]
+
+    if correct_letters == "":
+        print("\nNo correct letters are guessed so far.\n")
+    else:
+        print("\nThe letters " + correct_letters + " are in the answer.\n\n")
+
+    return
     
 def correct_guess():
     print("That is correct!, You WIN!!\n")
